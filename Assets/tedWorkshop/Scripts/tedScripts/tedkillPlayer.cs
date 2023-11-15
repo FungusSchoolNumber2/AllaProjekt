dd5cb72ef538ab5d3867479540afbc7cf5f1fba5
Ted Lundgren <a23tedlu@student.his.se>
2023-11-15 15:01:38 +0100

    Ted - Renamed Scripts
    slayful

--- Assets/tedWorkshop/Scripts/tedScripts/tedkillPlayer.cs
+++ Assets/tedWorkshop/Scripts/tedScripts/tedkillPlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Kill : MonoBehaviour
+public class tedkillPlayer : MonoBehaviour
 {
 
     public GameObject victim;
